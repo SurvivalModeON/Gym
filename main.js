99b568886cec4cabf415e4b1ae0effc66ee332af
VladUngurean <ghiena14@gmail.com>
2023-07-03 17:02:33 +0300

    gallery  (sass implement)

--- main.js
+++ main.js
@@ -1,37 +1,27 @@
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
 // MOBILE HEADER START 
 
+// mobile dropdown icon rotation
 $("#dropdown-button").click(function() {
   $('.rotate-delay').toggleClass('turn-ower-button');
 });
 
 // open and close mobile nav bar
-const targetDiv = document.getElementById("mobileSideNav");
+const mobileDropdownNav = document.getElementById("mobile-dropdown-nav");
 
 const dropDownBtn = document.getElementById("dropdown-button");
 const dropDownBtn1 = document.getElementById("dropdown-button1");
 dropDownBtn.onclick = function myFunction() {
-  if (targetDiv.style.opacity !== "0") {
-    targetDiv.style.opacity = "0";
-    dropDownBtn1.style.opacity = "0";
+  if (mobileDropdownNav.style.opacity !== "0") {
+    mobileDropdownNav.style.opacity = "0";
     dropDownBtn1.style.display = "none";
   } else {
-    targetDiv.style.opacity = "1";
-    dropDownBtn1.style.opacity = "1";
+    mobileDropdownNav.style.opacity = "1";
     dropDownBtn1.style.display = "block";
   }
 };
 
-// UL open and close 
+// UL open and close on click
 
 $('li.mobile-buiucani').click(function () {
   $(this).find('ul').toggle();
@@ -77,22 +67,111 @@ $('li.mobile-sculeanca').click(function () {
 // MOBILE HEADER END
 
 
-// carousels buttons 
-const buttons = document.querySelectorAll("[data-carousel-button]")
+// galllery start
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+$(document).ready(mySlider);
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+function mySlider() {
+    let imgNumber, 
+        sliderContainerWidth, 
+        imgContainer,
+        index,
+        flag = true,
+        speed = 600,
+        bullets = true,
+        auto = true ,
+        time = 30000 ;
+        
+    construction();
+    $(window).resize(construction);
+    if(auto){
+     let handle = setInterval(slideRight, time) ; 
+    }
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+    function construction() {
+        index = 1;
+        imgNumber = $('.images-container li').length;
+        sliderContainerWidth = Math.round($('#slider-container').width());
+        imgContainer = sliderContainerWidth * imgNumber;
+        $('.images-container').css("width", imgContainer);
+        $('.images-container li').css("width", sliderContainerWidth);
+        $('.images-container').css("margin-left", 0);
+        if (bullets == true) {
+            $('.bullets-container').html("");
+            for (i = 1; i <= imgNumber; i++) {
+                $('.bullets-container').append("<span class='bullet'></span>");
+            }
+            $('.bullet').eq(0).addClass('active');
+        }
+        $(".bullet").click(pagers);
+        $('.a-right').click(slideRight);
+        $('.a-left').click(slideLeft);
+    }
+
+    function pagers() {
+        if (!$(this).hasClass('active')) {
+            let bulletIndex = $(".bullets-container span").index(this) + 1;
+            index = bulletIndex;
+            $(".bullets-container").find(".bullet").removeClass("active").eq(bulletIndex - 1).addClass("active");
+            $('.images-container').animate({
+                marginLeft: -sliderContainerWidth * (bulletIndex - 1)
+            }, speed);
+        }
+    }
+
+    function slideRight() {
+        let imgContainerLeft = parseInt($('.images-container').css('margin-left'));
+        if (flag) {
+            if (imgContainerLeft == -sliderContainerWidth * (imgNumber - 1)) {
+                index = 1;
+                $('.images-container').animate({
+                    marginLeft: 0
+                }, speed, function() {
+                    flag = true;
+                })
+            } else {
+                index++;
+                $('.images-container').animate({
+                    marginLeft: '-=' + sliderContainerWidth
+                }, speed, function() {
+                    flag = true;
+                })
+            }
+            flag = false;
+            $(".bullets-container").find(".bullet").removeClass("active").eq(index - 1).addClass("active");
+        }
+    }
+
+    function slideLeft() {
+        let imgContainerLeft = parseInt($('.images-container').css('margin-left'));
+        clearInterval(slideRight, 3000);
+        if (flag) {
+            if (imgContainerLeft == 0 ) {
+                index = index + (imgNumber - 1);
+                $('.images-container').animate({
+                    marginLeft: -sliderContainerWidth * (imgNumber - 1) + 'px'
+                }, speed, function() {
+                    flag = true;
+                })
+            } else {
+                index--;
+                $('.images-container').animate({
+                    marginLeft: '+=' + sliderContainerWidth
+                }, speed, function() {
+                    flag = true;
+                })
+            }
+            flag = false;
+            $(".bullets-container").find(".bullet").removeClass("active").eq(index - 1).addClass("active");
+        }
+
+    }
+    $("#slider-container .arrow , .bullets-container").hover(function(){
+      clearInterval(handle);
+    },function(){
+      handle = setInterval(slideRight, time) ;
+    })
+  
+};
+
+// galllery start
