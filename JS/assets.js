6e8b48f67febfa067d3f42c27ad55b8d84307015
Vlad Ungurean <ghiena14@gmail.com>
2023-08-01 14:31:01 +0300

    headr footer code fix

--- JS/assets.js
+++ JS/assets.js
@@ -4,8 +4,3 @@ $(function () {
 $(function () {
   $("#for-footer").load("footer.html");
 });
-
-// document.getElementById("for-header").innerHTML =
-//   '<object type="type/html" data="header.html"></object>';
-
-// $("#for-header").load("header.html #header");
