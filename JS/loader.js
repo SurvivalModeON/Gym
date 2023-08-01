fad23c9e0a47970b75473ffb68658b427fd70f28
Vlad Ungurean <ghiena14@gmail.com>
2023-08-01 15:21:45 +0300

    loader for all pages

--- JS/loader.js
+++ JS/loader.js
@@ -1,9 +1,13 @@
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
     document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("body").style.overflowX = "hidden";
+    document.querySelector("body").style.overflowY = "hidden";
     document.querySelector("#loader").style.visibility = "visible";
   } else {
     document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.overflowX = "hidden";
+    document.querySelector("body").style.overflowY = "auto";
     document.querySelector("body").style.visibility = "visible";
   }
 };
